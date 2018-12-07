bb91bd68f9c95c431a32ae20ae61b42861286455
alexandrbublik <alexandrbublik@gmail.com>
2018-12-07 14:16:34 +0200

    add autoprefixer and etc

--- form-ideal-weight/gulpfile.js
+++ form-ideal-weight/gulpfile.js
@@ -2,6 +2,7 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 var csso        = require('gulp-csso');
+var autoprefixer = require('gulp-autoprefixer');
 
 // Static Server + watching scss/html files
 
@@ -10,6 +11,10 @@ var csso        = require('gulp-csso');
 gulp.task('style', function(){
     return gulp.src('sass/style.sass')
         .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions'],
+            cascade: false
+        }))
         .pipe(csso())
         .pipe(gulp.dest('css'))
 })
