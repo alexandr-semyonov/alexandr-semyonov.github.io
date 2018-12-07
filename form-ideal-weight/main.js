a7b14bfda0fed3db330570e1889198c41bb1e556
alexandrbublik <alexandrbublik@gmail.com>
2018-12-07 18:01:21 +0200

    return all

--- form-ideal-weight/main.js
+++ form-ideal-weight/main.js
@@ -1,33 +1,29 @@
 /* 
 Используй формулу: масса тела = 50 + 0,75 (Р – 150) + (В – 20) : 4, где Р — рост, а В — возраст в годах.
 */
-
+ 
 var result = document.querySelector("#result");
 var form = document.querySelector("form");
 
 function btnSubmit() {
   var ageValue = document.querySelector("#age").value;
   var growthValue = document.querySelector("#growth").value;
-  if (ageValue && growthValue) {
+  if (ageValue && growthValue){
     result.classList.add("result");
-    if (checkCorrecktnes(growthValue, ageValue)) {
-      result.value = idealWeight + ' кг.';
+    idealWeight = 50 + 0.75*(growthValue - 150) + (ageValue - 20);
+    if (idealWeight > 0){
+    result.value = idealWeight + ' кг.';
     } else {
       result.value = 'Пожалуйста правильность данных';
     }
   }
 }
 
-function checkCorrecktnes(growth, age) {
-  idealWeight = 50 + 0.75 * (growth - 150) + (age - 20);
-  return idealWeight > 0
-}
-
-function btnReset() {
+function btnReset(){
   result.classList.remove("result");
 }
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  btnSubmit();
+form.addEventListener("submit", function(event){  
+      event.preventDefault();
+      btnSubmit();
 });
