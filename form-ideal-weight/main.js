64d8b70906b8b1e86a47290d355b59b18514e28f
alexandrbublik <alexandrbublik@gmail.com>
2018-12-07 18:41:02 +0200

    Попытки отделить ЮАй

--- form-ideal-weight/main.js
+++ form-ideal-weight/main.js
@@ -1,29 +1,39 @@
 /* 
 Используй формулу: масса тела = 50 + 0,75 (Р – 150) + (В – 20) : 4, где Р — рост, а В — возраст в годах.
 */
- 
+
 var result = document.querySelector("#result");
 var form = document.querySelector("form");
 
 function btnSubmit() {
   var ageValue = document.querySelector("#age").value;
   var growthValue = document.querySelector("#growth").value;
-  if (ageValue && growthValue){
+  if (ageValue && growthValue) {
     result.classList.add("result");
-    idealWeight = Math.floor( 50 + 0.75*(growthValue - 150) + (ageValue - 20) );
-    if (idealWeight > 0){
-    result.value = idealWeight + ' кг.';
+    var idealWeight = calculateIdealWeight(ageValue, growthValue);
+    var correctResult = idealWeight + ' кг.';
+    var incorrectResult = 'Пожалуйста правильность данных';
+    if (idealWeight > 0) {
+      result.value = correctResult;
     } else {
-      result.value = 'Пожалуйста правильность данных';
+      result.value = incorrectResult;
     }
   }
 }
 
-function btnReset(){
+//function chechCorrectEntry(){
+//  
+//}
+
+function calculateIdealWeight(age, growth) {
+  return Math.floor(50 + 0.75 * (growth - 150) + (age - 20));
+}
+
+function btnReset() {
   result.classList.remove("result");
 }
 
-form.addEventListener("submit", function(event){  
-      event.preventDefault();
-      btnSubmit();
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  btnSubmit();
 });
