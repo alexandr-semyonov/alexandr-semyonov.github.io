2d88ca1a346dee29ee2c56465cf63e680a1fd046
alexandrbublik <alexandrbublik@gmail.com>
2018-12-07 14:28:06 +0200

    add alse gulp modules

--- form-ideal-weight/gulpfile.js
+++ form-ideal-weight/gulpfile.js
@@ -3,6 +3,8 @@ var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 var csso        = require('gulp-csso');
 var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var notify = require('gulp-notify');
 
 // Static Server + watching scss/html files
 
@@ -10,11 +12,17 @@ var autoprefixer = require('gulp-autoprefixer');
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('style', function(){
     return gulp.src('sass/style.sass')
+        .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(autoprefixer({
             browsers: ['last 3 versions'],
             cascade: false
         }))
+        .on("error", notify.onError({
+            message: "Error: <%= error.message %>",
+            title: "Error in style task"
+          }))
         .pipe(csso())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('css'))
 })
