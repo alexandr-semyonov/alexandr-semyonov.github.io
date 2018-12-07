b6fa439ecaa3a1757b1799db68fbf5ea6184271a
alexandrbublik <alexandrbublik@gmail.com>
2018-12-07 14:04:16 +0200

    edit gulpfile + change structure 07 12 2018 14 04

--- form-ideal-weight/gulpfile.js
+++ form-ideal-weight/gulpfile.js
@@ -1,24 +1,15 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+var csso        = require('gulp-csso');
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
 
-    browserSync.init({
-        server: "."
-    });
-
-    gulp.watch("./sass/**/*.{scss,sass}", ['sass']);
-    gulp.watch("*.html").on('change', browserSync.reload);
-});
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src("./sass/**/*.sass")
+gulp.task('style', function(){
+    return gulp.src('sass/style.sass')
         .pipe(sass())
-        .pipe(gulp.dest("./css"))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('default', ['serve']);
+        .pipe(csso())
+        .pipe(gulp.dest('css'))
+})
